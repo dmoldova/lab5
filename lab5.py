16f0b2da781af256e01bb7b95100c3b415ed24e8
root <dmoldova@usc.edu>
2018-02-23 15:45:19 -0800

    edit

--- lab5.py
+++ lab5.py
@@ -11,11 +11,11 @@ def Main():
 	#try:
 		GPIO.output(led,GPIO.HIGH)
 		time.sleep(2)
-		while True:
-			for x in range(1,6):
-				GPIO.output(led,GPIO.HIGH)
-				time.sleep(0.5)
-				GPIO.output(led,GPIO.LOW)
-				time.sleep(0.5)
+		#while True:
+		for x in range(1,6):
+			GPIO.output(led,GPIO.HIGH)
+			time.sleep(0.5)
+			GPIO.output(led,GPIO.LOW)
+			time.sleep(0.5)
 if __name__ == '__main__':
 	Main()
