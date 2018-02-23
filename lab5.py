16b512d63ea7eb5a8a8ed146d6cfe51a1632141a
root <dmoldova@usc.edu>
2018-02-23 15:24:20 -0800

    edit

--- lab5.py
+++ lab5.py
@@ -2,15 +2,17 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(11,GPIO.OUT)
+led = 11
+
+GPIO.setup(led,GPIO.OUT)
 
 #GPIO.setwarnings(False)
-def Main()
+def Main():
 	while True:
 		for x in range(1,6):
-			GPIO.output(11,1)
+			GPIO.output(led,1)
 			time.sleep(0.5)
-			GPIO.output(11,0)
+			GPIO.output(led,0)
 			time.sleep(0.5)
 
 if __name__ == '__main__':
