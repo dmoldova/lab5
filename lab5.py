72d355b21f1af1ea6e215028f16e60083f8d15d6
root <dmoldova@usc.edu>
2018-02-23 14:58:28 -0800

    edit 2

--- lab5.py
+++ lab5.py
@@ -2,10 +2,11 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(11,GPIO.OUT)
 
 while True:
 	for x in range(1,6):
-		GPIO.out(11,True)
+		GPIO.output(11,True)
 		time.sleep(0.5)
-	
+		GPIO.output(11,True)
+		time.sleep(0.5)
