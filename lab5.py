cf8d94dc910bbd9e1b8d77b9f47addfacf8b9dfd
root <dmoldova@usc.edu>
2018-02-23 15:19:05 -0800

    edit

--- lab5.py
+++ lab5.py
@@ -8,7 +8,7 @@ GPIO.setwarnings(False)
 
 while True:
 	for x in range(1,6):
-		GPIO.output(13,1)
+		GPIO.digitalWrite(13,1)
 		time.sleep(0.5)
-		GPIO.output(13,0)
+		GPIO.digitalWrite(13,0)
 		time.sleep(0.5)
